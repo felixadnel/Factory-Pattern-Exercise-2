8471d5dadbd5d3a93f1fc38e580d566cec609a67
Felix Adnel <felixadnel@Felixs-MacBook-Pro.local>
2023-09-30 03:18:29 -0400

    initial commit

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FactoryPatternExercise2
+{
+	public class MongoDataAccess : IDataAccess
+	{
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){ Name="Playstation", Price = 500},
+            new Product (){ Name="Controller", Price = 10},
+            new Product (){ Name="Robot", Price = 5000},
+            new Product (){ Name="Ipad", Price = 600},
+        };
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("i am reading data from Mongo Data Access.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("i am saving data from a Mongo Data Access.");
+        }
+    }
+}
+
---
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FactoryPatternExercise2
+{
+     class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){ Name="Playstation", Price = 500},
+            new Product (){ Name="Controller", Price = 10},
+            new Product (){ Name="Robot", Price = 5000},
+            new Product (){ Name="Ipad", Price = 600},
+        };
+
+        public List<Product> LoadData()
+        {
+           Console.WriteLine("i am reading data from SQL Data Access.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("i am saving data from a SQL Data Access.");
+        }
+    }
+}
+
